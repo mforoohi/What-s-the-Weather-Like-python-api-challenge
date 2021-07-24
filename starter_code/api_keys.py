36e74a1473c90629777cb5ee2335006c3a724785
Maryam <Foroohi@gmail.com>
2021-07-24 12:12:37 -0700

    third commit

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "021e21b97e69e74486caba6a7841762b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAxsZ82yl_UQpUtIPqhQ6Bgw8wxK8_ZwGU"
